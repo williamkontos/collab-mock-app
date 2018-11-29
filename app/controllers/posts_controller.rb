f4776addb46d1431a0d6b005cfe49960c926b28f
William Kontos <williamjkontos@gmail.com>
2018-11-29 15:22:06 -0500

    created a ui to create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,23 @@
 class PostsController < ApplicationController
+  before_action :redirect_if_not_signed_in, only: [:new]
+  
   def show
     @post = Post.find(params[:id])
+  end
+
+  def 
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to post_path(@post)
+    else
+      redirect_to root_path
+    end
   end
 
   def hobby
@@ -16,6 +33,11 @@ class PostsController < ApplicationController
   end
 
   private
+
+  def post_params
+    params.require(:post).permit(:content, :title, :category_id)
+                                .merge(user_id: current_user.id)
+  end
 
   def get_posts
     PostsForBranchService.new({
