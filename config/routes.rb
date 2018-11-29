59256aaf1a7a84de8f62f97917f9591867b82877
William Kontos <williamjkontos@gmail.com>
2018-11-28 21:45:13 -0500

    Displayed posts on homepage, generated posts controller declared routes for posts

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,13 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'signup', to: 'devise/registrations#new'
   end
+
+  resources :posts do 
+    collection do
+      get 'hobbby'
+      get 'study'
+      get 'team'
+    end
+  end
 end
+
